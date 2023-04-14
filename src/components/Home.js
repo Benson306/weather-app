a7137f43c0aeb364012fd74eba944241f9a38a8d
Benson Ndiwa <bnkimtai@gmail.com>
2023-04-14 12:42:35 +0300

    Hourly repoting Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,18 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 import { EvilIcons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function Home() {
 
   const [ data, setData ] = useState({})
   const [loading, setLoading ] = useState(true);
 
+  const [hourly, setHourly] = useState();
+
   useEffect(()=>{
 
-    fetch('https://api.weatherapi.com/v1/current.json?key=b0fa03de6eea43e497080000231304&q=Kisumu&aqi=no')
+    fetch('https://api.weatherapi.com/v1/current.json?key=b0fa03de6eea43e497080000231304&q=Nairobi&aqi=no')
     .then(res => res.json())
     .then(res =>{
       setData(res)
@@ -20,10 +23,18 @@ export default function Home() {
       console.log(res)
     })
 
+    fetch('https://api.weatherapi.com/v1/forecast.json?key=b0fa03de6eea43e497080000231304&q=Nairobi&days=1&aqi=no&alerts=no')
+    .then(res => res.json())
+    .then(res =>{
+      setHourly(res)
+      setLoading(false)
+      console.log(res)
+    })
+
   },[])
   return (
     <View style={styles.container}>
-      <View style={{flexDirection:'row', marginTop:10, width: 200, justifyContent:'center', alignItems:'center', alignSelf:'center'}}>
+      <View style={{flexDirection:'row', marginTop:40, width: 200, justifyContent:'center', alignItems:'center', alignSelf:'center'}}>
         <View>
           <EvilIcons name="location" size={24} color="#3399ff" />
         </View>
@@ -51,7 +62,7 @@ export default function Home() {
           </View>
 
           <View style={{alignItems:'flex-end'}}>
-            <Text style={{color:'white', marginTop: 50, fontSize:18}}>{data.location.localtime.slice(0,10)}</Text>
+            <Text style={{color:'white', marginTop: 45, fontSize:18}}>{data.location.localtime.slice(0,10)}</Text>
             <Text style={{color:'gray', fontSize:16}}>{data.location.localtime.slice(11,16)}</Text>
           </View>
           
@@ -61,7 +72,7 @@ export default function Home() {
       
         {!loading && <View style={{marginHorizontal:10, marginTop:50, flexDirection:'row', justifyContent:'space-around'}}>
           <View style={{alignItems:'center'}}>
-              <MaterialCommunityIcons name="weather-windy" size={47} color="white" />
+              <MaterialCommunityIcons name="weather-windy" size={47} color="gray" />
               <Text style={{color:'white', marginTop:10, fontWeight:'bold'}}>{data.current.wind_kph} km/h</Text>
               <Text style={{color:'gray'}}>wind</Text>
           </View>
@@ -73,11 +84,37 @@ export default function Home() {
           </View>
 
           <View style={{alignItems:'center'}}>
-              <MaterialCommunityIcons name="temperature-celsius" size={47} color="#3399ff" />
+              <MaterialCommunityIcons name="temperature-celsius" size={45} color="#3399ff" />
               <Text style={{color:'white', marginTop:10, fontWeight:'bold'}}>{data.current.temp_c}</Text>
               <Text style={{color:'gray'}}>Temp</Text>
           </View>
       </View>
+        }
+
+      <Text style={{color:'white', padding: 20, fontSize:20, fontWeight:'bold', marginTop:20}}>Hourly Forecast</Text>
+        {
+          !loading &&
+
+          <ScrollView horizontal={true}>
+            {
+              hourly.forecast.forecastday[0].hour.map( hr =>
+                {
+                  if(Number(hr.time.slice(11,13)) > Number(data.location.localtime.slice(11,13))){
+                    return (
+                      <View style={{backgroundColor:'#80ccff', height:150, elevation:3, shadowOffset:{width:1, height:1}, shadowColor:'#00b8e6', shadowRadius:2, shadowOpacity:0.3, padding:10, borderRadius:15, marginHorizontal:5, width: 120, alignItems:'center'}}>
+                        <Text style={{fontSize:16}}>{hr.time.slice(11,16)}</Text>
+                        <Image source={{ uri: `https:${hr.condition.icon}`}} style={{width: 80, height: 80, objectFit:'scale-down'}}/>
+                        <Text style={{fontSize:10, textTransform:'capitalize', color:'#030c3b'}}>{hr.condition.text}</Text>
+                        <Text style={{color:'gray'}}>{hr.temp_c} ℃</Text>
+                      </View>
+                    
+                    )
+                  }
+                }
+                )
+            }
+          </ScrollView>
+            
         }
         
     </View>
@@ -88,11 +125,12 @@ const styles= StyleSheet.create({
     container:{
         flex:1,
         backgroundColor: '#00001a',
-        marginTop:27
+        marginTop:0
     },
     dropdown:{
       width:150,
       color:'white',
-      borderBottomWidth:1
+      borderBottomWidth:1,
+      alignSelf:'center'
     }
 })
